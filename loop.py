b787349ee86693ea23e393c1e7e3c1e02de6715c
sujaydas <sujaydas17595@gmail.com>
2026-08-09 21:21:33 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,22 +1,35 @@
-sum = 0
+# sum = 0
 
-for i in range(1, 101):
-    if i % 2 == 0:
-        sum = sum + i
+# for i in range(1, 101):
+#     if i % 2 == 0:
+#         sum = sum + i
 
-print("Sum =", sum)
-
-
+# print("Sum =", sum)
 
 
 
-num = int(input("Enter a number: "))
 
-reverse = 0
 
-while num > 0:
-    digit = num % 10
-    reverse = reverse * 10 + digit
-    num = num // 10
+# num = int(input("Enter a number: "))
 
-print("Reverse =", reverse)
+# reverse = 0
+
+# while num > 0:
+#     digit = num % 10
+#     reverse = reverse * 10 + digit
+#     num = num // 10
+
+# print("Reverse =", reverse)
+
+
+
+
+
+
+x = 0
+
+for i in range(1, 6):
+    for j in range(i):
+        x = x + 1
+
+print(x)
