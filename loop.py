c72a4c79db6bdc2764053799f3ea5e50cc41c074
sujaydas <sujaydas17595@gmail.com>
2026-08-09 21:20:26 +0530

    reverse  in loop

--- loop.py
+++ loop.py
@@ -5,3 +5,18 @@ for i in range(1, 101):
         sum = sum + i
 
 print("Sum =", sum)
+
+
+
+
+
+num = int(input("Enter a number: "))
+
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+print("Reverse =", reverse)
