4295e756e2413cb96a90cf7372263ab8ab35f808
sujaydas <sujaydas17595@gmail.com>
2026-08-23 21:52:47 +0530

    Update recursion2.py

--- recursion2.py
+++ recursion2.py
@@ -45,3 +45,19 @@ def fun(n):
     return fun(n-1) + fun(n-2)
 
 print(fun(5))
+
+
+
+
+
+
+
+
+def fun(n):
+    if n == 0:
+        return
+    if n % 2 == 0:
+        print(n, end=" ")
+    fun(n-1)
+
+fun(6)
