726e6a2df1dda4023555882464e23f7811c8efcd
sujaydas <sujaydas17595@gmail.com>
2026-09-08 23:12:09 +0530

    Update adv gate.py

--- adv gate.py
+++ adv gate.py
@@ -1,51 +1,61 @@
-# def f(n):
-#     if n <= 1:
-#         return n
-#     return f(n-1) + f(n-2)
-
-# print(f(6))
-
-
-
-
-# def f(a):
-#     a[1] = a[0] + a[2]
-#     return a
-
-# x = [2, 4, 6]
-# y = f(x)
-
-# print(x)
-# print(y)
-
-
-
-
-
-# def f(a, b=10):
-#     return a + b
-
-# x = f(5)
-# y = f(5, 20)
-
-# print(x + y)
-
-
-
-# count = 0
-
-# for i in range(4):
-#     for j in range(i):
-#         count += 1
-
-# print(count)
-
-
-
-
 def f(n):
-    if n == 0:
-        return 0
-    return n + f(n-2)
+    if n <= 1:
+        return n
+    return f(n-1) + f(n-2)
 
-print(f(9))
+print(f(6))
+
+
+
+
+def f(a):
+    a[1] = a[0] + a[2]
+    return a
+
+x = [2, 4, 6]
+y = f(x)
+
+print(x)
+print(y)
+
+
+
+
+
+def f(a, b=10):
+    return a + b
+
+x = f(5)
+y = f(5, 20)
+
+print(x + y)
+
+
+
+count = 0
+
+for i in range(4):
+    for j in range(i):
+        count += 1
+
+print(count)
+
+
+
+# def f(n):
+#     if n == 0:
+#         return 0
+#     return n + f(n-2)
+
+# print(f(9))
+
+
+
+def f(d):
+    d["x"] = d["a"] + d["b"]
+    return d
+
+d = {"a": 2, "b": 3}
+x = f(d)
+
+print(d["x"])
